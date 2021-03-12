5cad9bc5a5c85346524d4ff831e6287aedee931d
= <tranminhtri9090@gmail.com>
2021-03-12 21:43:03 +1000

    Adding getNote lambda function

---
+++ get.js
@@ -0,0 +1,20 @@
+import handler from './libs/handler-lib';
+import dynamoDB from './libs/dynamodb-lib';
+
+export const main = handler(async (event, context) => {
+    const params = {
+        TableName: process.env.tableName,
+        Key: {
+            userId: "123",
+            noteId: event.pathParameters.id,
+        }
+    };
+
+    const result = await dynamoDB.get(params);
+
+    if (!result.Item) {
+        throw new Error("Can't find the Item");
+    };
+
+    return result.Item;
+});
