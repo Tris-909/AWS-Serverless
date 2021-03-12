889711cda1c18f7e77f3c77d29177624012b9bb5
= <tranminhtri9090@gmail.com>
2021-03-12 23:31:30 +1000

    Adding deleteNote Lambda Function

--- delete.js
+++ delete.js
@@ -2,22 +2,15 @@ import handler from './libs/handler-lib';
 import dynamoDB from './libs/dynamodb-lib';
 
 export const main = handler(async (event, context) => {
-    const data = JSON.parse(event.body);
     const params = {
         TableName: process.env.tableName,
         Key: {
             userId: "123",
             noteId: event.pathParameters.id,
         },
-        UpdateExpression: "SET content = :content, attachment = :attachment",
-        ExpressionAttributeValues: {
-            ":content": data.content,
-            ":attachment": data.attachment
-        },
-        ReturnValues: "ALL_NEW",
     };
 
-    await dynamoDB.update(params);
+    await dynamoDB.delete(params);
 
     return {
         status: true
